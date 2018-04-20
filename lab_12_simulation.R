94d9fe11b7676ca3230365fe81d8bd81ce5a89fd
Mary Safy <msafy@andrew.cmu.edu>
2018-04-19 20:03:14 -0400

    added model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,4 +4,9 @@ generate_data = function(n, p){
   return(list(covariates, responses))
 }
 
-
+model_select = function(covariates, responses, cutoff) {
+  mod = lm(responses~covariates)
+  sub.cov = covariates[covariates<cutoff]
+  mod2 = lm(responses~mod)
+  return(summary(mod2)$coefficients[,4])
+}
