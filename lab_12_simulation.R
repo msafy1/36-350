3113ffd44bde771461ff05a9a386c4cfead1e877
Mary Safy <msafy@andrew.cmu.edu>
2018-04-19 20:16:34 -0400

    added run_simulation function and examples

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -10,3 +10,22 @@ model_select = function(covariates, responses, cutoff) {
   mod2 = lm(responses~mod)
   return(summary(mod2)$coefficients[,4])
 }
+
+run_simulation = function(n_trials, n, p, cutoff){
+  data = generate_data(n, p)
+  mod = model_select(data[[0]]~data[[1]])
+  sum = summary(mod)
+  hist(sum[,4])
+}
+
+#p-values seem to be uniformly distributed.
+
+run_simulation(n=100, p=10)
+run_simulation(n=1000, p=10)
+run_simulation(n=10000, p=10)
+run_simulation(n=100, p=20)
+run_simulation(n=1000, p=20)
+run_simulation(n=10000, p=20)
+run_simulation(n=100, p=50)
+run_simulation(n=1000, p=50)
+run_simulation(n=10000, p=50)
